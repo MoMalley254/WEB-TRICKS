95a3c044495f8d9149fede102f78c3b09987185b
MoMalley254 <101170756+MoMalley254@users.noreply.github.com>
2022-12-20 10:10:21 +0300

    Simple text animation pre-loader

---
+++ WEB TEST/test.js
@@ -0,0 +1,8 @@
+//Preloader
+    /*var loader = document.getElementById("preloader");
+    window.addEventListener("load" , function() {
+        loader.style.display = "none";
+    })*/
+//Preloader
+//Uncomment script to remove animation on page load
+
